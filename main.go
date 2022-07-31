4f15585b1a1c8dc51211fc329e58ce0d5aa27754
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2022-07-31 23:13:17 +0530

    Routes and Controllers Added

--- main.go
+++ main.go
@@ -17,6 +17,60 @@ func GetDatabase() (*sql.DB, error) {
 	return db, err
 }
 
+func GetStocks(c *gin.Context) {
+	// TODO: Get stocks from database
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "get users",
+	})
+}
+
+func GetStock(c *gin.Context) {
+	symbol := c.Param("symbol")
+
+	// TODO: Get stock from database
+
+	if len(symbol) < 2 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid Stock"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"symbol": symbol,
+	})
+
+}
+
+func CreateStock(c *gin.Context) {
+	// TODO: Create stock in database
+
+	c.JSON(http.StatusCreated, gin.H{
+		"message": "Crater Created",
+	})
+}
+
+func UpdateStock(c *gin.Context) {
+	symbol := c.Param("symbol")
+
+	// TODO: Update stock in database
+
+	c.JSON(http.StatusOK, gin.H{
+		"symbol":  symbol,
+		"message": "Crater Updated",
+	})
+}
+
+func DeleteStock(c *gin.Context) {
+	symbol := c.Param("symbol")
+
+	// TODO: Delete stock from database
+
+	c.JSON(http.StatusOK, gin.H{
+		"symbol":  symbol,
+		"message": "Crater Updated",
+	})
+}
+
 func main() {
 	err := godotenv.Load(".env")
 	if err != nil {
@@ -33,6 +87,12 @@ func main() {
 		panic(err)
 	}
 	fmt.Println("Successfully connected to PlanetScale!")
+
+	r.GET("/stocks/", GetStocks)
+	r.GET("/stocks/:symbol", GetStock)
+	r.POST("/stocks/", CreateStock)
+	r.PUT("/stocks/:symbol", UpdateStock)
+	r.DELETE("/stocks/:symbol", DeleteStock)
 
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
